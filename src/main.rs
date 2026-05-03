57c1767cdea25e8a024855cffe78d1c67661d4a1
Hassan Fakih <hasanfakih@cmail.carleton.ca>
2026-05-03 14:47:02 -0400

    Added basic way to render pieces

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,25 @@ pub mod board;
 #[macroquad::main("MyGame")]
 async fn main() {
     let board_texture = load_texture("assets/board.png").await.unwrap();
+    let bb_texture = load_texture("assets/bb.png").await.unwrap();
+
     let bit_board = BitBoard::new();
     print_board(&bit_board.white_king);
-
-    let test = 1u64 << Square::B3 as u64;
-    print_board(&test);
 
     loop {
         draw_texture(&board_texture, 0.0, 0.0, WHITE);
         request_new_screen_size(768.0, 768.0);
+        draw_piece(0.0, 0.0, &bb_texture);
         next_frame().await
     }
+}
+
+
+fn draw_piece(x: f32, y: f32, texture: &Texture2D) {
+    let params = DrawTextureParams {
+        dest_size: Some(vec2(96.0, 96.0)),
+        ..Default::default()
+    };
+
+    draw_texture_ex(texture, x, y, WHITE, params);
 }
