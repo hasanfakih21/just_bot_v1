b86db76b9dccbccd4e673d0b6b359bebbe495ad4
Hassan Fakih <hasanfakih@cmail.carleton.ca>
2026-05-21 20:37:38 -0400

    Added best move from transposition table to the beginning of the order moves list

--- src/search.rs
+++ src/search.rs
@@ -88,6 +88,7 @@ pub fn negamax(depth: usize, board: &mut Board, mut alpha: i32, beta: i32, nodes
     for m in order_moves(board).iter() {
         if board.make_move(*m).is_ok() {
             legal_moves += 1;
+
             let score = -negamax(depth - 1, board, -beta, -alpha, nodes, ply + 1);
             board.unmake_move();
             if score > max {
@@ -151,7 +152,16 @@ pub fn quiesce(board: &mut Board, mut alpha: i32, beta: i32, nodes: &mut i32, _p
 pub fn order_moves(board: &mut Board) -> MoveList {
     //We want to sort the moves based on most valuable victim / least valuable attacker
     //Sort captures based on (attacked piece value - attacking piece value)
+    let mut full_list = MoveList::new();
     let mut captures: MoveList = board.generate_moves(MoveGenKind::Captures);
+    let mut best_move: Option<Move> = None;
+
+    //Want to add the best move from the transposition table if it exists to the beginning of the list
+    if let Some(e) = board.tt.get_entry(board.board_state.hash) && board.board_state.hash == e.get_key() {
+            let bm = e.get_best_move();
+            full_list.push(bm);
+            best_move = Some(bm);
+    }
 
     captures.iter_mut().into_slice().sort_by_key(|m| {
         let attacker = board.get_piece_at_square(m.get_from()).unwrap().1;
@@ -164,18 +174,12 @@ pub fn order_moves(board: &mut Board) -> MoveList {
 
     let quiet = board.generate_moves(MoveGenKind::Quiet);
     let pawn_promos = board.generate_moves(MoveGenKind::NonCapturePromotions);
-    let mut full_list = MoveList::new();
 
     for m in captures.iter().chain(pawn_promos.iter()).chain(quiet.iter()) {
+        if let Some(bm) = best_move && *m == bm {continue;}
         full_list.push(*m);
     }
 
-    //Want to add the best move from the transposition table if it exists to the beginning of the list
-    // captures.append(&mut others);
-    // if let Some(e) = board.tt.get_entry(board.board_state.hash) && board.board_state.hash == e.get_key() {
-    //         let best_move = e.get_best_move();
-    //         captures.insert(0, best_move);
-    //     }
     full_list
 }
 
