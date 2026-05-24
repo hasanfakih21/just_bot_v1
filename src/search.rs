8e91ebac1ed8be960cf3460b8d1862eed34fd5b4
Hassan Fakih <hasanfakih@cmail.carleton.ca>
2026-05-23 21:04:39 -0400

    Added a check for checks in QS and a seperate order_noisy_moves function

--- src/search.rs
+++ src/search.rs
@@ -192,8 +192,15 @@ pub fn quiesce(board: &mut Board, mut alpha: i32, beta: i32, nodes: &mut i32, pl
         alpha = best_score;
     }
 
-    for m in order_moves(board).iter() {
-        if !m.get_kind().is_quiet() && board.make_move(*m).is_ok() {
+    let move_list =
+    if board.king_in_check() {
+        order_moves(board)
+    } else {
+        order_noisy_moves(board)
+    };
+
+    for m in move_list.iter() {
+        if board.make_move(*m).is_ok() {
             let score = -quiesce(board, -beta, -alpha, nodes, ply + 1);
             board.unmake_move();
 
@@ -223,6 +230,53 @@ pub fn quiesce(board: &mut Board, mut alpha: i32, beta: i32, nodes: &mut i32, pl
     }
 
     best_score
+}
+
+pub fn order_noisy_moves(board: &mut Board) -> MoveList {
+    //We want to sort the moves based on most valuable victim / least valuable attacker
+    //Sort captures based on (attacked piece value - attacking piece value)
+    let mut full_list = MoveList::new();
+    let mut captures: MoveList = board.generate_moves(MoveGenKind::Captures);
+    let mut best_move: Option<Move> = None;
+
+    //Want to add the best move from the transposition table if it exists to the beginning of the list
+    if let Some(e) = board.tt.get_entry(board.board_state.hash)
+        && board.board_state.hash == e.get_key()
+    {
+        let bm = e.get_best_move();
+        full_list.push(bm);
+        best_move = Some(bm);
+    }
+
+    captures.iter_mut().into_slice().sort_by_key(|m| {
+        let attacker = board.get_piece_at_square(m.get_from()).unwrap().1;
+        let victim = match m.get_kind() {
+            MoveKind::EnPassant => {
+                board
+                    .get_piece_at_square(Square::from(m.get_to() as usize ^ 8))
+                    .unwrap()
+                    .1
+            }
+            _ => board.get_piece_at_square(m.get_to()).unwrap().1,
+        };
+        Reverse(victim.value() - attacker.value())
+    });
+
+    let pawn_promos = board.generate_moves(MoveGenKind::NonCapturePromotions);
+
+    for m in captures
+        .iter()
+        .chain(pawn_promos.iter())
+    {
+        if let Some(bm) = best_move
+            && *m == bm
+        {
+            continue;
+        }
+        full_list.push(*m);
+    }
+
+    full_list
 }
 
 pub fn order_moves(board: &mut Board) -> MoveList {
